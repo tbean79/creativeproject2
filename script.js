569e024b1c1e3d9d784bc641210c3045ad3eb32e
Tyler Dean <tdbone7@gmail.com>
2019-10-09 21:49:52 +0000

    javascript api parsing happens quicker

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 /*global fetch*/
 
-/*global require*/
 document.getElementById("findCard").addEventListener("click", function(event) {
   event.preventDefault();
   
-  document.getElementById("magicResults").innerHTML = "";
+
   
   const value = document.getElementById("magicInput").value;
   if (value === "")
     return;
-  console.log(value);
+  //console.log(value);
 
-const url = "https://api.magicthegathering.io/v1/cards";
+const url = "https://api.magicthegathering.io/v1/cards?name=" + value;
   fetch(url)
   .then(function(response) {
       return response.json();
@@ -19,34 +18,25 @@ const url = "https://api.magicthegathering.io/v1/cards";
         //console.log(json);
         let stats = "";
         
-        var foundMatch = false;
-        
-        for (let i = 0; i < json.cards.length; i++) {
-            if (json.cards[i].name === value) {
-                stats += "<p>Name: " + json.cards[i].name + "</br>";
-                stats += "Rarity: " + json.cards[i].rarity + "</br>";
-                stats += "Description: " + json.cards[i].text + "</br>";
-                if (json.cards[i].flavor != null) {
-                    stats += json.cards[i].flavor + "</br>";
-                }
-                foundMatch = true;
-                break;
-            }
-            
-        }
-        
-        if (foundMatch == false)
+        if (json.cards.length === 0)
         {
             stats = "Not Found";
             document.getElementById("magicResults").innerHTML = stats;
         }
         else
         {
-            //console.log(stats);
-            document.getElementById("magicResults").innerHTML = stats;
+            stats += "<p>Name: " + json.cards[0].name + "</br>";
+            stats += "Rarity: " + json.cards[0].rarity + "</br>";
+            stats += "Description: " + json.cards[0].text + "</br>";
+            if (json.cards[0].flavor != null) {
+                stats += json.cards[0].flavor + "</br>";
+            }
+        
+        //console.log(stats);
+        document.getElementById("magicResults").innerHTML = stats;
+        
         }
-    
-    });
+    }); 
 });
 
 
@@ -60,34 +50,71 @@ document.getElementById("displayPhoto").addEventListener("click", function(event
     return;
   console.log(value);
 
-const url = "https://api.magicthegathering.io/v1/cards";
+const url = "https://api.magicthegathering.io/v1/cards?name=" + value;
+console.log(url);
   fetch(url)
   .then(function(response) {
       return response.json();
     }).then(function(json) {
-        //console.log(json);
+        console.log(json);
         let photo = "";
         
-        var foundMatch = false;
-        
-        for (let i = 0; i < json.cards.length; i++) {
-            if (json.cards[i].name === value) {
-                photo += "<img src = \"" + json.cards[i].imageUrl + "\"></p>";
-                foundMatch = true;
-                break;
-            }
-            
-        }
-        
-        if (foundMatch == false)
+        if (json.cards.length === 0)
         {
             photo = "Not Found";
             document.getElementById("magicResults").innerHTML = photo;
         }
         else
         {
-            //console.log(stats);
+            photo += "<img src = \"" + json.cards[0].imageUrl + "\"></p>";
+                
+        
+            console.log(photo);
             document.getElementById("magicResults").innerHTML = photo;
+        }
+    
+    });
+});
+
+document.getElementById("sortType").addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  document.getElementById("magicResults").innerHTML = "";
+  
+  const value = document.getElementById("magicInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+
+const url = "https://api.magicthegathering.io/v1/cards?type=" + value;
+  fetch(url)
+  .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        //console.log(json);
+        let list = "";
+        
+        if (json.cards.length === 0)
+        {
+            list = "Not Found";
+            document.getElementById("magicResults").innerHTML = list;
+        }
+        else
+        {
+        
+        for (let i = 0; i < json.cards.length; i++) {
+            list += "<p>Name: " + json.cards[i].name + "</br>";
+            list += "Rarity: " + json.cards[i].rarity + "</br>";
+            list += "Description: " + json.cards[i].text + "</br>";
+            if (json.cards[i].flavor != null) {
+                list += json.cards[i].flavor + "</br>";
+            }
+            list += "</br>";
+            
+        }
+        
+            //console.log(stats);
+            document.getElementById("magicResults").innerHTML = list;
         }
     
     });
